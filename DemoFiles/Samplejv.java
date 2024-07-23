296ec56730c303159f77aa04e1caf61c60a7225b
rensuiii <crenzzz123@gmail.com>
2024-07-23 16:33:55 +0800

    Add sample Python, PHP, and Java greetings to DemoFolder

---
+++ DemoFiles/Samplejv.java
@@ -0,0 +1,6 @@
+// Sample.java
+public class Samplejv {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- DemoFiles/samplepython.py
+++ DemoFiles/samplepython.py
@@ -1,1 +1,2 @@
+# sample.py
 print("Hello, World!")
